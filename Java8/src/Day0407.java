84641a5124c899aa013dcf45ea9a4abdede687e3
yn-s04 <yn-s04@DESKTOP-7TBF8RP>
2020-03-17 15:40:00 +0900

    자바 기본문법 Chapter02 연습문제 01번

---
+++ Java8/src/Day0407.java
@@ -0,0 +1,17 @@
+package java_oh;
+
+import java.util.Scanner;
+
+public class Day0407 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("이름, 학번, 토플점수를 입력하세요: ");
+		String name = sc.next();
+		int no = sc.nextInt();
+		int score = sc.nextInt();
+		
+		System.out.println("나의 이름은 " + name + " 입니다.");
+		System.out.println("학번은 " + no + "입니다.");
+		System.out.println("그리고 토플 점수는 " + score + "점 입니다.");
+	}
+}
