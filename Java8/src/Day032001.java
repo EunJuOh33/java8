2c141b70f6cd5012b684a33d9bc1d67f7bc8659f
yn-s04 <yn-s04@DESKTOP-7TBF8RP>
2020-03-23 10:36:31 +0900

    for문

---
+++ Java8/src/Day032001.java
@@ -0,0 +1,14 @@
+
+
+public class Day032001 {
+	public static void main(String[] args) {
+		int sum = 0;
+		int i = 0;
+		
+		for(i=1; i<=100; i++) {
+			sum +=i;
+		}
+		
+		System.out.println("1부터 10까지의 합: " + sum);
+	}
+}	
