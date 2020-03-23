a272e05dbeb2d3695ff9413a91692fdf87612c3e
yn-s04 <yn-s04@DESKTOP-7TBF8RP>
2020-03-23 12:41:36 +0900

    for문 예제 - 1부터 9까지, hello! 출력

---
+++ Java8/src/java8/Day032003.java
@@ -0,0 +1,14 @@
+package java8;
+
+public class Day032003 {
+
+	public static void main(String[] args) {
+		for(int i=0; i<10; i++) {
+			System.out.println(i);
+		}
+
+		for(int i=0; i<5; i++){
+			System.out.println("hello!");
+		}
+	}
+}
