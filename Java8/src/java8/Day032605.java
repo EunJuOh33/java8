1f5824d15930d01d633af5008df715f8717adcaf
yn-s04 <yn-s04@DESKTOP-7TBF8RP>
2020-03-26 10:38:32 +0900

    배열 : 다른 객체로 변환

---
+++ Java8/src/java8/Day032605.java
@@ -0,0 +1,24 @@
+package java8;
+
+public class Day032605 {
+	public static void main(String[] args) {
+		int[] arr1 = {10, 20, 30, 40, 50};
+//		arr1 = {100, 200, 300, 400, 500};	//중괄호 초기화는 최초의 한 번만 사용가능합니다.
+		
+		int[] arr2 = new int[5];	//0, 0, 0, 0, 0
+//		arr2 = {10, 20, 30, 40, 50};
+		
+		int[] arr3 = new int[] {10, 20, 30, 40, 50};	//크기가 5인 배열
+//		arr3 = {100, 200, 300, 400, 500};
+		
+		String str1 = "안녕하세요";
+		str1 = "hello";	//동일한 형태의 다른 객체를 대입할 수 있다.
+		
+		arr3 = new int[] {100, 200, 300, 400, 500};	//배열도 동일한 형태의 다른 객체를 대입할 수 있습니다.
+		for(int i=0; i<arr3.length; i++) {
+			System.out.println(arr3[i]);
+		}
+		
+		arr2 = new int[10];	//다른 객체로 변환이 가능합니다.
+	}
+}
