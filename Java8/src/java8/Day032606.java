e78813472797c757e5939f3eced559e3b52cf28f
yn-s04 <yn-s04@DESKTOP-7TBF8RP>
2020-03-26 11:18:59 +0900

    배열 : 다차원 배열

---
+++ Java8/src/java8/Day032606.java
@@ -0,0 +1,29 @@
+package java8;
+
+public class Day032606 {
+	public static void main(String[] args) {
+		int[] arr1 = { 10, 20, 30, 40, 50};
+		int[] [] ary1 = { {10, 20}, {30, 40}, {50, 60}, {70, 80}};	//new int[4][2];와 동일한 의미
+		
+		System.out.println(ary1.length);	//첫번째 괄호의 크기를 나타낸다. 4
+		System.out.println(ary1[0].length);	//2
+		System.out.println(ary1[1].length);	//2
+		System.out.println(ary1[2].length);	//2
+		System.out.println(ary1[3].length);	//2
+//		System.out.println(ary1[4].length);	//4는 없다. 오류
+		
+		for(int i=0; i<ary1.length; i++) {			//ary1.length의 값은 4. i<4를 넣어도 됨
+			for(int j=0; j<ary1[i].length; j++) {	//ary1[i].length의 값은 2. j<2를 넣어도 됨
+				System.out.println(ary1[i][j]);
+			}
+		}
+		System.out.println();
+		
+		int[][] ary2 = {{10,20,30}, {40}, {50,60}};
+		for(int i=0; i<3; i++) {
+			for(int j=0; j<1; j++) {
+				System.out.println(ary2[i][j]);
+			}
+		}
+	}
+}
