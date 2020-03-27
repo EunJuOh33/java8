0523c23013eda86bd6f51542916796f58dabe087
yn-s04 <yn-s04@DESKTOP-7TBF8RP>
2020-03-27 13:58:09 +0900

    연습 문제 : 학점 계산기

---
+++ Java8/src/java8/Day032710.java
@@ -0,0 +1,63 @@
+package java8;
+
+import java.util.Scanner;
+
+public class Day032710 {
+	public static void main(String[] args) {
+		/*
+		 * 출석20%, 과제20%*/
+		int a = 0, b = 0, c = 0, d = 0; 
+		char grade = 0;
+		Scanner sc = new Scanner(System.in);
+
+		
+		do {
+			if(a < 0 || a > 100) {
+				System.out.println("입력 범위를 초과하였습니다.");
+			}
+			System.out.print("출석 점수는 몇 점 입니까? : ");
+			a = sc.nextInt();
+		} while (a < 0 || a > 100);
+		
+		do {
+			if(b < 0 || b > 100) {
+				System.out.println("입력 범위를 초과하였습니다.");
+			}
+			System.out.print("과제 점수는 몇 점 입니까? : ");
+			b = sc.nextInt();
+		} while (b < 0 || b > 100);
+		
+		do {
+			if(c < 0 || c > 100) {
+				System.out.println("입력 범위를 초과하였습니다.");
+			}
+			System.out.print("중간고사 점수는 몇 점 입니까? : ");
+			c = sc.nextInt();
+		} while (c < 0 || c > 100);
+		
+		do {
+			if(d < 0 || d > 100) {
+				System.out.println("입력 범위를 초과하였습니다.");
+			}
+			System.out.print("기말고사 점수는 몇 점 입니까? : ");
+			d = sc.nextInt();
+		} while (d < 0 || d > 100);
+		
+		
+		int score = (int)(a / 100 * 20 + b / 100 * 20 + c / 100 * 30 + d / 100 * 30);
+		System.out.print("총 점은 " + score + "입니다.");
+		
+		if(score >= 90) {
+			grade = 'A';
+		} else if(score >= 80) {
+			grade = 'B';
+		} else if(score >= 70) {
+			grade = 'D';
+		} else if(score >= 60) {
+			grade = 'F';
+		}
+		System.out.println("당신의 학점은 " + grade + "입니다.");
+		
+		sc.close();
+	}
+}
