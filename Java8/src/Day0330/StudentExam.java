6de81fcd9ccdf6adf554873f2ad5c8e1a98d8519
yn-s04 <yn-s04@DESKTOP-7TBF8RP>
2020-03-30 14:46:05 +0900

    student 변수

---
+++ Java8/src/Day0330/StudentExam.java
@@ -0,0 +1,9 @@
+package Day0330;
+
+public class StudentExam {
+	public static void main(String[] args) {
+		Student student = new Student();	//Student라는 클래스 파일이 있기 때문에 Student형 변수를 만들 수 있다.
+											//변수의 이름은 원하는 대로 수정할 수 있다.(지금은 student)
+		Person person = new Person();
+	}
+}
