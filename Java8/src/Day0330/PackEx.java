d1c272715ff0ec968cb14421080c8ca6f5e89563
yn-s04 <yn-s04@DESKTOP-7TBF8RP>
2020-03-30 14:54:34 +0900

    java.util 패키지 내의 클래스 사용

---
+++ Java8/src/Day0330/PackEx.java
@@ -0,0 +1,11 @@
+package Day0330;
+
+import java.util.Random;	//java.util 패키지 내의 Random클래스를 사용한다.
+//import java.util.*		//java.util 패키지  내의 모든 클래스를 사용한다.
+
+public class PackEx {
+	public static void main(String[] args) {
+		Random rd = new Random();
+//		java.util.Random rd = new java.util.Random();	//원래는 이렇게 써야한다.
+	}
+}
