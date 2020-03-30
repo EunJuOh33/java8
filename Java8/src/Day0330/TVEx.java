ca3f850064dc74ef13c349d2f4279d9f946d86dd
yn-s04 <yn-s04@DESKTOP-7TBF8RP>
2020-03-30 15:58:10 +0900

    실습

---
+++ Java8/src/Day0330/TVEx.java
@@ -0,0 +1,11 @@
+package Day0330;
+
+public class TVEx {
+	public static void main(String[] args) {
+		/*
+		 * LG에서 만든 2017년형 32인치 TV
+		 */
+		TV myTV = new TV("LG", 2017, 32);
+		myTV.show();
+	}
+}
