e5cefcb7d0c3f4b0e1b00a7d0a7f207a2348fd75
yn-s04 <yn-s04@DESKTOP-7TBF8RP>
2020-03-31 11:37:23 +0900

    StringTokenizer 클래스

--- Java8/src/Day0331/TokenEx.java
+++ Java8/src/Day0331/TokenEx.java
@@ -13,7 +13,13 @@ public class TokenEx {
 		
 		while(st.hasMoreTokens()) {
 			String temp = st.nextToken();	//첫 번째 토큰을 반환하고(temp로 반환) 처음으로 돌아간다.
-			System.out.println(temp);
+			System.out.println(temp);	//2020 03 31
+		}
+		
+		
+		String[] arr = now.split("/");
+		for(String str : arr) {
+			System.out.println(str);
 		}
 	}
 }
