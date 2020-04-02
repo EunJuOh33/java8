36d8de5f52e36cf66d643b176394bcf35b8748ca
yn-s04 <yn-s04@DESKTOP-7TBF8RP>
2020-04-02 09:56:08 +0900

    인터페이스 구현

---
+++ Java8/src/Day0402/TV.java
@@ -0,0 +1,29 @@
+package Day0402;
+
+public class TV implements RemoteControl {
+	int volume;
+
+	@Override
+	public void turnOn() {
+		System.out.println("TV를 켭니다.");
+	}
+
+	@Override
+	public void turnOff() {
+		System.out.println("TV를 끕니다.");
+	}
+
+	@Override
+	public void setVolume(int volume) {
+		this.volume = volume;
+		System.out.println("볼륨을 " + volume + "으로 변경합니다.");
+	}
+	
+	@Override
+	public void setMute(boolean mute) {
+		if (mute)
+			System.out.println("TV를 음소거 상태로 설정합니다.");
+		else
+			System.out.println("TV의 음소거 상태를 해제합니다.");
+	}
+}
