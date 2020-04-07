c931d05200e8b787fc4b3fd6ee0f25f1e55f6155
yn-s04 <yn-s04@DESKTOP-7TBF8RP>
2020-04-07 16:01:30 +0900

    Predicate 함수적 인터페이스

---
+++ Java8/src/Day0407/Student2.java
@@ -0,0 +1,28 @@
+package Day0407;
+
+public class Student2 {
+	private String name;
+	private String sex;
+	private int score;
+	
+	//»ý¼ºÀÚ
+	public Student2(String name, String sex, int score) {
+		super();
+		this.name = name;
+		this.sex = sex;
+		this.score = score;
+	}
+
+	
+	public String getName() {
+		return name;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public int getScore() {
+		return score;
+	}	
+}
