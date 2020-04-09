609dd587c6bfc4651f6d54534df303feca2fc5d8
yn-s04 <yn-s04@DESKTOP-7TBF8RP>
2020-04-09 11:35:30 +0900

    interrupt() 메소드

---
+++ Java8/src/Day0409/InterruptEx.java
@@ -0,0 +1,14 @@
+package Day0409;
+
+public class InterruptEx {
+	public static void main(String[] args) {
+		Thread thread = new PrintThread2();
+		thread.start();
+		
+		try {
+				Thread.sleep(1000);
+		} catch (InterruptedException e) {}
+		
+		thread.interrupt();
+	}
+}
